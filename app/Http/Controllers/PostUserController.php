cf5b97ee35ab74160b159e6a36e0cb3c04bf7ca7
sitaaraman <sitaaraman2003@gmail.com>
2025-11-06 11:56:36 +0530

    user commit

--- app/Http/Controllers/PostUserController.php
+++ app/Http/Controllers/PostUserController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class PostUserController extends Controller
 {
-    //
+    public function index(){
+        return view('postuser.index');
+    }
+
+    public function create(){
+        return view('postuser.create');
+    }
+
+    public function store(Request $request){
+
+        $request->validate([
+            'fullname' => 'required',
+            'email' => 'required|email',
+            'profile' => 'required',
+            'password' => 'required|min:4',
+        ]);
+        return view('postuser.create');
+    }
 }
