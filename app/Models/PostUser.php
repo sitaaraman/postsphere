d3d979fb2d5c1657f8ea2f3c4867da826a85406f
sitaaraman <sitaaraman2003@gmail.com>
2025-11-06 11:35:51 +0530

    user commit

--- app/Models/PostUser.php
+++ app/Models/PostUser.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostUser extends Model
 {
-    //
+    protected $table = "post_users";
+    
+    protected $fillable = [
+        'fullname',
+        'email',
+        'profile',
+        'password',
+        'slug'
+    ];
 }
