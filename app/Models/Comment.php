7205d9ec707ee980e5aae516a38fc5222f107e79
sitaaraman <sitaaraman2003@gmail.com>
2025-11-12 11:41:38 +0530

    post commit

---
+++ app/Models/Comment.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $table = 'comments';
+    
+    protected $fillable = [
+        'post_id',
+        'post_user_id',
+        'comment',
+    ];
+}
